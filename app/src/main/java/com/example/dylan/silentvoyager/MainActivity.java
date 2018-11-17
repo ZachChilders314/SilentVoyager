97af0433c98743252ff29f850ae9cc3ee6db5d65
burghporter314 <burghporter314@gmail.com>
2018-11-17 11:50:01 -0500

    Added Video Functionality for onCreate for Main Intent

--- app/src/main/java/com/example/dylan/silentvoyager/MainActivity.java
+++ app/src/main/java/com/example/dylan/silentvoyager/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.dylan.silentvoyager;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private VideoView introVideoView;
+    private Button btnLogin;
+
+    private String PATH;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PATH = "android.resource://"+getPackageName()+"/"+R.raw.fly_by;
+        introVideoView = (VideoView) findViewById(R.id.introVideoView);
+        btnLogin       = (Button) findViewById(R.id.btnLogin);
+
+        introVideoView.setVideoPath(this.PATH);
+
+        introVideoView.requestFocus();
+        introVideoView.start();
+
+        introVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+            }
+        });
+
     }
+
 }
