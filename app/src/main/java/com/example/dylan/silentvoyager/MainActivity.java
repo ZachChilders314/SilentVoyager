b662f9000acf9f80b669b75a32698734993473e5
burghporter314 <burghporter314@gmail.com>
2018-11-17 23:06:02 -0500

    Added Run-Time Permission Functionality and a Handler for onResume for the Main Activity

--- app/src/main/java/com/example/dylan/silentvoyager/MainActivity.java
+++ app/src/main/java/com/example/dylan/silentvoyager/MainActivity.java
@@ -1,17 +1,41 @@
 package com.example.dylan.silentvoyager;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    
+    private final String[] ACTIVITY_PERMISSIONS = {
+            Manifest.permission.INTERNET,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.RECORD_AUDIO
+    };
+
+    private final int REQUEST_INTERNET = 0;
+    private final int REQUEST_LOCATION = 1;
+    private final int REQUEST_AUDIO    = 2;
+    private final int REQUEST_MULTIPLE = 3;
 
     private VideoView introVideoView;
     private Button btnLogin;
 
+    private final String ERROR_MESSAGE_PERMISSIONS = "NOTICE: All Permissions Required to Proceed!";
+    private final String SUCCESS_MESSAGE_PERMISSIONS = "NOTICE: All Permissions Granted.";
     private String PATH;
 
     @Override
@@ -20,12 +44,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        PATH = "android.resource://"+getPackageName()+"/"+R.raw.fly_by;
+        PATH = "android.resource://" + getPackageName() + "/" + R.raw.fly_by;
+
         introVideoView = (VideoView) findViewById(R.id.introVideoView);
         btnLogin       = (Button) findViewById(R.id.btnLogin);
 
         introVideoView.setVideoPath(this.PATH);
-
         introVideoView.requestFocus();
         introVideoView.start();
 
@@ -36,6 +60,81 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                if(getNonGrantedPermissions(ACTIVITY_PERMISSIONS).length != 0) {
+                    Toast.makeText(getApplicationContext(), ERROR_MESSAGE_PERMISSIONS,
+                            Toast.LENGTH_LONG).show();
+
+                    String[] permissionsDenied
+                            = getNonGrantedPermissions(ACTIVITY_PERMISSIONS);
+
+                    synchronized(this) {
+                        requestPermissions(permissionsDenied);
+                    }
+
+                } else {
+                    Toast.makeText(getApplicationContext(), SUCCESS_MESSAGE_PERMISSIONS,
+                            Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
     }
 
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+        introVideoView.start();
+
+    }
+
+    private String[] getNonGrantedPermissions(String[] permissions) {
+
+        List<String> nonGrantedPermissions = new ArrayList<String>();
+
+        for(String item : permissions) {
+
+            if(ContextCompat.checkSelfPermission(this,
+                    item) != PackageManager.PERMISSION_GRANTED) {
+                nonGrantedPermissions.add(item);
+            }
+        }
+
+        return nonGrantedPermissions.toArray(new String[nonGrantedPermissions.size()]);
+    }
+
+    private void requestPermissions(String[] permissions) {
+
+        ActivityCompat.requestPermissions(this,
+                permissions, REQUEST_MULTIPLE);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch(requestCode) {
+            case REQUEST_INTERNET: {
+                //TODO
+                break;
+            }
+            case REQUEST_LOCATION: {
+                //TODO
+                break;
+            }
+            case REQUEST_AUDIO: {
+                //TODO
+                break;
+            }
+            case REQUEST_MULTIPLE: {
+                //TODO
+                break;
+            }
+        }
+    }
 }
