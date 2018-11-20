c7aa9aa06ea0a4d325ac1927c1a9e0df6c16eb04
burghporter314 <burghporter314@gmail.com>
2018-11-19 19:59:50 -0500

    Added Video View code using onCreate() and resume() methods associated with activities

--- app/src/main/java/com/example/dylan/silentvoyager/LoginActivity.java
+++ app/src/main/java/com/example/dylan/silentvoyager/LoginActivity.java
@@ -1,13 +1,44 @@
 package com.example.dylan.silentvoyager;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private String PATH;
+    private VideoView introVideoView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Bundle data = getIntent().getExtras();
+        PATH = data.getString("PATH");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        introVideoView = (VideoView) findViewById(R.id.introVideoView);
+
+        introVideoView.setVideoPath(this.PATH);
+        introVideoView.requestFocus();
+        introVideoView.start();
+
+        introVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+            }
+        });
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        introVideoView.start();
+    }
+
 }
