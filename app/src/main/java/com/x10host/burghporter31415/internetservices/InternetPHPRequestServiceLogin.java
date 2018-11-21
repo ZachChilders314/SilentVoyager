81bd77fab16fb9de2db1a68bc433ce29d0672a9c
burghporter314 <burghporter314@gmail.com>
2018-11-20 23:29:59 -0500

    Service that sends a post request to a php file on my server and retrieves the result using Pending Intent. The idea was to use callbacks for the original activity that started the service.

--- app/src/main/java/com/x10host/burghporter31415/internetservices/InternetPHPRequestServiceLogin.java
+++ app/src/main/java/com/x10host/burghporter31415/internetservices/InternetPHPRequestServiceLogin.java
@@ -1,31 +1,47 @@
-package com.x10host.burghporter31415.services;
+package com.x10host.burghporter31415.internetservices;
 
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.util.Log;
 
 import com.x10host.burghporter31415.webconnector.FormPost;
 import com.x10host.burghporter31415.webconnector.MethodType;
 import com.x10host.burghporter31415.webconnector.PHPPage;
 
-public class InternetPHPRequestService extends Service {
+public class InternetPHPRequestServiceLogin extends Service {
 
-    public InternetPHPRequestService() {
+    private static final String TAG = "com.x10host";
+
+    private PendingIntent data;
+
+    public InternetPHPRequestServiceLogin() {
 
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
+        final Bundle extras = intent.getExtras();
+        data = intent.getParcelableExtra("pendingIntent");
+
         Runnable r = new Runnable() {
             @Override
             public void run() {
-                
+
                 synchronized(this) {
+
+                    Intent resultIntent = new Intent();
+
                     try {
 
-                        String username = txtUsername.getText().toString();
-                        String password = txtPassword.getText().toString();
+                        String username = extras.getString("username");
+                        String password = extras.getString("password");
+
+                        String BASE_URL = extras.getString("BASE_URL");
+                        String RELATIVE_URL = extras.getString("RELATIVE_URL");
 
                         FormPost<String, String> connection = new FormPost<String, String>();
 
@@ -36,12 +52,25 @@ public class InternetPHPRequestService extends Service {
 
                         String result = connection.submitPost(page, MethodType.POST);
 
-                    } catch(Exception e) {
+                        resultIntent.putExtra("result", result);
+                        resultIntent.putExtra("username", username);
+                        resultIntent.putExtra("password", password);
 
+                        data.send(InternetPHPRequestServiceLogin.this, 200, resultIntent);
+
+                    } catch(Exception e) {
+                        Log.i(TAG, "Failed");
                     }
+
+
                 }
             }
         };
+
+        Thread thread = new Thread(r);
+        thread.start();
+
+        return Service.START_STICKY;
 
     }
 
